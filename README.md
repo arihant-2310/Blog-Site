862ed8bcde9f8748cf741b619c8e378105e1e920
arihant-2310 <arihant2310@gmail.com>
2020-05-22 05:16:25 +0530

    adds articles basic template

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Blog-Site
 To run : python manage.py runserver
+To create app:python manage.py startapp articles
--- djangonautic/urls.py
+++ djangonautic/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from . import views #from current directory import views.py file
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('articles/',include('articles.urls')),
     path('about/',views.about), #url to about page
     path('',views.homepage), #url to home page
 ]
