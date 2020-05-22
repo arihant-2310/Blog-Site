a17cf43f2ebc07c6b50217f4b235e1b445e5c6f2
arihant-2310 <arihant2310@gmail.com>
2020-05-22 10:44:15 +0530

    added styles to articles list page

--- djangonautic/urls.py
+++ djangonautic/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path,include
 from . import views #from current directory import views.py file
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,3 +9,5 @@ urlpatterns = [
     path('about/',views.about), #url to about page
     path('',views.homepage), #url to home page
 ]
+
+urlpatterns += staticfiles_urlpatterns()
