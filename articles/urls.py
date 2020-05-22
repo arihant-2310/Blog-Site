6e696b761321b3b1483f947558b1edcf0bdb041a
arihant-2310 <arihant2310@gmail.com>
2020-05-22 11:51:52 +0530

    adds hrefs to articles

--- articles/urls.py
+++ articles/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views #from current directory import views.py file
 
+app_name = 'articles'
+
 urlpatterns = [
-    path('',views.articles_list), #url to articles home page
+    path('',views.articles_list,name="list"), #url to articles home page
+    url(r'^(?P<slug>[\w-]+)/$', views.article_details,name="detail"), #match article slug
 ]
