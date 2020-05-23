e57eebb0a52fcc5862d64254e34f1b7574a7ee06
arihant-2310 <arihant2310@gmail.com>
2020-05-23 12:45:28 +0530

    adds create article functionality

--- articles/urls.py
+++ articles/urls.py
@@ -6,5 +6,6 @@ app_name = 'articles'
 
 urlpatterns = [
     path('',views.articles_list,name="list"), #url to articles home page
+    path('create',views.create_article,name="create"), #url to create articles page
     url(r'^(?P<slug>[\w-]+)/$', views.article_details,name="detail"), #match article slug
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Article
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def articles_list(request):
@@ -10,4 +11,7 @@ def article_details(request,slug):
     # return HttpResponse(slug)
     article = Article.objects.get(slug=slug)
     return render(request,'articles/article_detail.html',{'article':article})
-    
+
+@login_required(login_url="/accounts/login")
+def create_article(request):
+    return render(request,'articles/create_article.html')
