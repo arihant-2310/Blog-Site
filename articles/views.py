b2ca7fb58e3783b453241eec0000bbe65e836889
arihant-2310 <arihant2310@gmail.com>
2020-05-24 11:06:54 +0530

    associate user with the articles

--- articles/views.py
+++ articles/views.py
@@ -19,6 +19,9 @@ def create_article(request):
         form = forms.CreateArticle(request.POST,request.FILES)
         if form.is_valid():
             #save to db
+            instance = form.save(commit=False)
+            instance.author = request.user
+            instance.save()
             return redirect('articles:list')
     else:
         form = forms.CreateArticle()
