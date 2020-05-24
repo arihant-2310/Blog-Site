78a56b631eb0745c7e49f696f3f73a64ac92732b
arihant-2310 <arihant2310@gmail.com>
2020-05-24 11:23:22 +0530

    adds create and signup buttons

--- djangonautic/urls.py
+++ djangonautic/urls.py
@@ -4,13 +4,14 @@ from . import views #from current directory import views.py file
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.conf import settings
+from articles import views as articles_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/',include('accounts.urls')),
     path('articles/',include('articles.urls')),
     path('about/',views.about), #url to about page
-    path('',views.homepage), #url to home page
+    path('',articles_view.articles_list,name="home"), #url to home page
 ]
 
 urlpatterns += staticfiles_urlpatterns()
